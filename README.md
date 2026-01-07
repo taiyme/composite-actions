77772a18a30340c40f792a8ae05da18180fe0625
taiyme <53635909+taiyme@users.noreply.github.com>
2026-01-07 21:44:39 +0900

    feat: setup project

---
+++ README.md
@@ -0,0 +1,7 @@
+# taiyme/composite-actions
+
+[![Release](https://img.shields.io/github/v/release/taiyme/composite-actions?display_name=tag&style=flat)](https://github.com/taiyme/composite-actions/releases/latest)
+[![License](https://img.shields.io/github/license/taiyme/composite-actions?style=flat)](./LICENSE)
+[![Donate](https://img.shields.io/badge/donate-%3C3-f96854?style=flat)](https://taiy.me/to/donate)
+
+taiyがよく使う、複合アクションをまとめたものです。
---
+++ eslint.config.ts
@@ -0,0 +1,31 @@
+import taiymeConfig from '@taiyme/eslint-config';
+import tsEslintParser from '@typescript-eslint/parser';
+import gitignore from 'eslint-config-flat-gitignore';
+import { defineConfig } from 'eslint/config';
+import globals from 'globals';
+
+const files = ['**/*.{js,ts}'];
+
+export default defineConfig([
+  gitignore(),
+  {
+    files,
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      parser: tsEslintParser,
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files,
+    extends: [
+      taiymeConfig.configs.typescript,
+      taiymeConfig.configs.stylistic,
+    ],
+  },
+]);
